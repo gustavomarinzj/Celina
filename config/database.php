389f640353d37b4b1a438b12561e5d6b436e4934
Gustavo Marín <123376755+gustavomarinzj@users.noreply.github.com>
2026-06-04 16:53:26 -0400

    Add files via upload

--- config/database.php
+++ config/database.php
@@ -18,8 +18,8 @@ class Database {
 			);
 			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			$this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-		} catch(PDOException $exception) {
-			echo "Error de conexión: " . $exception->getMessage();
+		} catch(PDOException $e) {
+			echo "Error de conexión: " . $e->getMessage();
 		}
 
 		return $this->conn;
